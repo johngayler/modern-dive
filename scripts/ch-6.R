bbdee536a1bbc21a6eee47f36d986239e2b68b74
John Gayler <john.gayler@intrepidgroup.travel>
2020-05-12 17:55:59 +1000

    Commit ch6 - start

---
+++ scripts/ch-6.R
@@ -0,0 +1,10 @@
+# Chapter 6 - Multiple Regression
+
+# Load required packages
+library(tidyverse)
+library(moderndive)
+library(skimr)
+library(ISLR)
+
+# 6.1 One numerican and one categorical explanatory variable
+
